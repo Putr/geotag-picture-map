8254273f871a28de7cdd8943da0d9b4f22e11890
Rok Andrée <rok@andree.si>
2014-05-18 14:37:52 +0200

    [] Adds support for subfolders in original_imgs

--- process_imgs.php
+++ process_imgs.php
@@ -4,6 +4,7 @@
 // CONFIGURATION
 // 
 // 
+$start = time();
 
 // Path where your photos are located
 define('ORIGINAL_PATH', 'original_img/');
@@ -20,57 +21,85 @@ define('IMG_WIDTH', 300);
 //
 require_once 'SimpleImage.php';
 
-$files = scandir(ORIGINAL_PATH);
+$fileList         = array();
+$fileList['root'] = array();
 
-$data = array();
+foreach (new DirectoryIterator(ORIGINAL_PATH) as $fileInfo) {
+    if($fileInfo->isDot()) continue;
 
+    if ($fileInfo->isDir()) {
+    	$subDirFiles = array();
+    	foreach (new DirectoryIterator($fileInfo->getPathname()) as $fileInfo2) {
+    		if($fileInfo2->isDot()) continue;
+    		$subDirFiles[] = $fileInfo2->getFilename();
+    	}
+    	$dir = str_replace(' ', '_', $fileInfo->getFilename());
+    	$fileList[$dir] = $subDirFiles;
+    }
+
+    if ($fileInfo->getFilename() == '.gitignore') continue;
+
+    if ($fileInfo->isFile()) {
+    	$fileList['root'][] = $fileInfo->getFilename();
+    }
+}
+
+$data  = array();
 $count = array(
 	'total' => 0,
-	'err' => 0
+	'err'   => 0
 );
-foreach ($files as $fileName) {
+foreach ($fileList as $dir => $files) {
+	foreach ($files as $fileName) {
 
-	$count['total']++;
+		$count['total']++;
 
-	if ($fileName == '.' || $fileName == '..' || $fileName == '.gitignore' ) {
-		continue;
+		if ($dir == 'root') {
+			$filePath = ORIGINAL_PATH . $fileName;
+		} else {
+			$filePath = sprintf('%s%s/%s', ORIGINAL_PATH, $dir, $fileName);
+		}
+
+		$exifData = exif_read_data ( $filePath );
+
+		if (!isset($exifData['GPSLatitude']) || !isset($exifData['GPSLongitude'])) {
+			clog($dir, $fileName, 'This image has no GPS data');
+			$count['err']++;
+			continue;
+		}
+
+		// Build EXIF database
+		// 
+		$lat = $exifData['GPSLatitude'];
+		$long = $exifData['GPSLongitude'];
+
+		$ext = pathinfo($filePath, PATHINFO_EXTENSION);
+		$newFileName = sprintf('%s_%s.%s', $dir, substr(md5($fileName), 0, 8), $ext);
+
+		$data[] = array(
+			'lat' => compileCoordinate($lat),
+			'lng' => compileCoordinate($long),
+			'img' => $newFileName
+		);
+
+		$image = new SimpleImage();
+		$image->load( $filePath);
+		$image->resizeToWidth(IMG_WIDTH);
+		$image->save(WEB_IMG_PATH . $newFileName);
+		echo ".";
 	}
-	//clog($fileName, ' Starting processing ... ');
-
-	$exifData = exif_read_data ( ORIGINAL_PATH . $fileName );
-
-	if (!isset($exifData['GPSLatitude']) || !isset($exifData['GPSLongitude'])) {
-		clog($fileName, 'This image has no GPS data');
-		$count['err']++;
-		continue;
-	}
-
-	// Build EXIF database
-	// 
-	$lat = $exifData['GPSLatitude'];
-	$long = $exifData['GPSLongitude'];
-
-	$data[] = array(
-		'lat' => compileCoordinate($lat),
-		'lng' => compileCoordinate($long),
-		'img' => $fileName
-	);
-	//clog($fileName, 'Found coordinates');
-
-	// Create thumbnail
-	//clog($fileName, 'Generating thumbnail');
-	$image = new SimpleImage();
-	$image->load( ORIGINAL_PATH . $fileName);
-	$image->resizeToWidth(IMG_WIDTH);
-	$image->save(WEB_IMG_PATH . $fileName);
 }
 
 $data = json_encode($data);
 
 file_put_contents('web/locations.json', $data);
 
-clog('', sprintf('Found %s images, %s had errors so only %s were processed', $count['total'], $count['err'], $count['total'] - $count['err']));
+echo sprintf('%sFound %s images, %s had errors so only %s were processed. %s', PHP_EOL, $count['total'], $count['err'], $count['total'] - $count['err'], PHP_EOL);
+$end = time();
 
+$time = $end - $start;
+$avg = $time / $count['total'];
+echo sprintf('Execution took %s seconds, on average %s per image. %s', $time, $avg, PHP_EOL);
 
 function compileCoordinate($data) {
 
@@ -85,8 +114,8 @@ function compileCoordinate($data) {
 
 }
 
-function clog($filename, $msg) {
-	echo sprintf('[%s] %s %s', $filename, $msg, PHP_EOL);
+function clog($dir, $filename, $msg) {
+	echo sprintf('%s[%s][%s] %s %s', PHP_EOL, $dir, $filename, $msg, PHP_EOL);
 }
 
 
